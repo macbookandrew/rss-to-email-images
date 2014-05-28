b55752edc84b1d16e6b29864376fd39eed4d7102
macbookandrew <macbookandrew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-28 01:54:05 +0000

    inital release
    git-svn-id: http://plugins.svn.wordpress.org/rss-to-email-images/trunk@922092 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== RSS-to-Email Images ===
+Contributors: macbookandrew
+Requires at least: 3.0
+Tested up to: 3.9
+Stable tag: 1.0
+Tags: RSS
+License: GPL v3
+License URI: http://choosealicense.com/licenses/gpl-v3/
+
+Fixes large images in RSS-to-email campaigns
+
+== Description ==
+Fixes large images in RSS-to-email campaigns by scaling them to a smaller size to fit most mobile screen sizes. Based on Chris Coyier’s post on CSSTricks (http://css-tricks.com/dealing-content-images-email/)
+
+== Installation ==
+1. Install the plugin.
+2. Append "?feed=email" to the end of your RSS feed URL (www.example.com/feed/ would become www.example.com/feed/?feed=email).
+3. Use the new URL in Feedburner or your RSS-to-email distributor of choice.
+
+== Changelog ==
+1.0: basic plugin
---
+++ rss-to-email-images.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Plugin Name: RSS-to-Email Images
+ * Plugin URI: http://code.andrewrminion.com/rss-to-email-images/
+ * Description: Fixes large images in RSS-to-email campaigns; based on http://css-tricks.com/dealing-content-images-email/
+ * Version: 1.0
+ * Author: AndrewRMinion Design
+ * Author URI: http://andrewrminion.com
+ * License: GPL3
+ */
+
+/*  Copyright 2014 AndrewRMinion Design (http://andrewrminion.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 3, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// create a new RSS feed specifically for use with RSS-to-email
+function rss_to_email_custom_feeds() {
+  add_feed('email', 'new_feed_template');
+}
+add_action('init', 'rss_to_email_custom_feeds');
+
+
+// flush rewrite rule cache on activation/deactivation
+function rss_to_email_activate() {
+	flush_rewrite_rules();
+}
+register_activation_hook(__FILE__, 'rss_to_email_activate');
+
+function rss_to_email_deactivate() {
+	flush_rewrite_rules();
+}
+register_deactivation_hook(__FILE__, 'rss_to_email_deactivate');
+
+// get the email feed template
+function new_feed_template() {
+  add_filter('the_content_feed', 'rss_to_email_feed_image_magic');
+  include(ABSPATH . '/wp-includes/feed-rss2.php' );
+}
+
+
+// change the HTML in the feed content
+function rss_to_email_feed_image_magic($content) {
+  // Weirdness we need to add to strip the doctype with later.
+  $content = '<div>' . $content . '</div>';
+  $doc = new DOMDocument();
+  $doc->LoadHTML($content);
+  $images = $doc->getElementsByTagName('img');
+  foreach ($images as $image) {
+    $image->removeAttribute('height');
+    $image->setAttribute('width', '320');
+  }
+  // Strip weird DOCTYPE that DOMDocument() adds in
+  $content = substr($doc->saveXML($doc->getElementsByTagName('div')->item(0)), 5, -6);
+  return $content;
+}
+
+
+?>
